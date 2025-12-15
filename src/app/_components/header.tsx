c83b9e52c2f250ac5438425a615c2d5f2b366689
SoulHiro <victormts.s1@gmail.com>
2025-12-14 21:16:16 -0300

    feat: Add Radix UI dialog component, implement theme provider and toggle, and enhance header with sidebar menu for improved navigation

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,26 +1,46 @@
 'use client'
+
+import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
+import { MenuIcon, MessageCircleIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import SidebarMenu from '@/app/_components/sidebar-menu'
+import Link from 'next/link'
+import { ThemeToggle } from '@/components/theme-toggle'
 
-import { LogInIcon, MenuIcon } from 'lucide-react'
-
-const HeaderComponent = () => {
-  const handlelogin = async () => {
-    await authClient.signIn.social({ provider: 'google' })
-  }
+const Header = () => {
   return (
-    <header className="flex items-center justify-between px-5 py-6">
-      <h1>BarberFlow</h1>
-      <div>
-        <Button variant="outline" size="icon" onClick={handlelogin}>
-          <LogInIcon />
+    <header className="bg-background flex items-center justify-between px-5 py-6">
+      <h1>Barberflow</h1>
+      <div className="flex items-center gap-2">
+        <ThemeToggle />
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/chat">
+            <MessageCircleIcon />
+          </Link>
         </Button>
-        <Button variant="outline" size="icon">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[370px] p-0">
+            <SheetHeader className="border-b px-5 py-6 text-left">
+              <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+            </SheetHeader>
+            <SidebarMenu />
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   )
 }
 
-export default HeaderComponent
+export default Header
